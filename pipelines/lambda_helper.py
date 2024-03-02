f440b669bfa45344ad887dc6400668040df63898
nathanmeth <nathan.metheny@fort-companies.com>
2024-03-01 19:47:32 -0500

    fargate deployment

--- pipelines/lambda_helper.py
+++ pipelines/lambda_helper.py
@@ -33,3 +33,55 @@ def endpoint_handler(event, context):
         "body": json.dumps("Created Endpoint!"),
         "other_key": "example_value",
     }
+
+
+def deploy_to_fargate(event, context):
+    ecs_client = boto3.client('ecs')
+    ecr_image_url = event["image_url"]
+
+    task_definition_response = ecs_client.register_task_definition(
+        family='rl-trading-system',
+        networkMode='awsvpc',
+        containerDefinitions=[
+            {
+                'name': 'rl-trading-v1',
+                'image': ecr_image_url,
+                'essential': True,
+                'portMappings': [
+                    {
+                        'containerPort': 80,
+                        'hostPort': 80,
+                        'protocol': 'tcp'
+                    },
+                ],
+                'memory': 512,
+                'cpu': 256
+            },
+        ],
+        requiresCompatibilities=['FARGATE'],
+        cpu='256',
+        memory='512'
+    )
+
+    task_definition_arn = task_definition_response \
+        ['taskDefinition']['taskDefinitionArn']
+
+    run_task_response = ecs_client.run_task(
+        cluster='dev-cluster',
+        launchType='FARGATE',
+        taskDefinition=task_definition_arn,
+        count=1,
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [
+                    'subnet-0bd001b0367876f50',
+                ],
+                'assignPublicIp': 'DISABLED'
+            }
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Deployed to Fargate"),
+    }
