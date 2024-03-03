8c6a6d1a8a5ef2570f7ebdc84f0efca371747aeb
nathanmeth <nathan.metheny@fort-companies.com>
2024-03-03 10:02:45 -0500

    separating endpoint and fargate lambda

--- pipelines/fargate_helper.py
+++ pipelines/fargate_helper.py
@@ -1,41 +1,8 @@
 import json
 import boto3
 
-# reference: https://github.com/aws/amazon-sagemaker-examples/blob/main/sagemaker-pipelines/tabular/lambda-step/sagemaker-pipelines-lambda-step.ipynb
 
-def deploy_to_sagemaker_endpoint(event, context):
-    """ """
-    sm_client = boto3.client("sagemaker")
-
-    model_name = event["model_name"]
-
-    endpoint_config_name = event["endpoint_config_name"]
-    endpoint_name = event["endpoint_name"]
-
-    create_endpoint_config_response = sm_client.create_endpoint_config(
-        EndpointConfigName=endpoint_config_name,
-        ProductionVariants=[
-            {
-                "InstanceType": "ml.m4.xlarge",
-                "InitialVariantWeight": 1,
-                "InitialInstanceCount": 1,
-                "ModelName": model_name,
-                "VariantName": "AllTraffic",
-            }
-        ],
-    )
-    create_endpoint_response = sm_client.create_endpoint(
-        EndpointName=endpoint_name, EndpointConfigName=endpoint_config_name
-    )
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Created Endpoint!"),
-        "other_key": "example_value",
-    }
-
-
-def deploy_to_fargate(event, context):
+def endpoint_handler(event, context):
     ecs_client = boto3.client('ecs')
     ecr_image_url = event["image_url"]
 
